791d93a10a13815e7f0baa2d4c1e7c64efd3ce0c
ShakilaCodes <shakilamuhammad@gmail.com>
2020-03-29 20:11:11 -0400

    Updates

---
+++ precache-manifest.9d4e7c652f6deb622e2f24b0879a2d66.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7aa57951af4150499449b6b859310ac8",
+    "url": "/starshipfly/index.html"
+  },
+  {
+    "revision": "589134453e3a6e3484ba",
+    "url": "/starshipfly/static/css/2.4c9505d4.chunk.css"
+  },
+  {
+    "revision": "2e005e5ce0bf923ec5c0",
+    "url": "/starshipfly/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "589134453e3a6e3484ba",
+    "url": "/starshipfly/static/js/2.101022d2.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/starshipfly/static/js/2.101022d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e005e5ce0bf923ec5c0",
+    "url": "/starshipfly/static/js/main.e801f750.chunk.js"
+  },
+  {
+    "revision": "ad6c2b756bbd156c0e6b",
+    "url": "/starshipfly/static/js/runtime-main.5ac53f2a.js"
+  }
+]);
--- src/components/swapi.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-  baseURL: `https://swapi.co/api/starships`
-});
-
-export const API_URL = "https://swapi.co/api/starships";
-export const SEARCH_URL = "https://swapi.co/api/starships/?search=";
